895450d3a1835d63424fb9ac969f3e1107e0a40b
siddharth_uxc <siddharthjhamail@gmail.com>
2026-07-05 17:42:45 +0530

    Load zepto inventory dataset

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("welcome to ecommerce inventory dashboard")
+import pandas as pd
+
+df = pd.read_csv("data/zepto_v2.csv", encoding="latin1")
+
+
+print(df.head())
+print("\nshape:", df.shape)
+print("\ncolumns:", df.columns)
+print("\ninfo:")
+print(df.info())
