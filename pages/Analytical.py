00f4550daea96414add4dced2fadcc1008d532e1
siddharth_uxc <siddharthjhamail@gmail.com>
2026-07-15 23:42:41 +0530

    Added stock analysis and low stock product insighs

--- pages/Analytical.py
+++ pages/Analytical.py
@@ -53,3 +53,75 @@ st.plotly_chart(
     use_container_width=True
 )
 
+# ==================================================
+# AVERAGE DISCOUNT BY CATEGORY
+# Calculate the average discount percentage
+# for each category.
+# ==================================================
+
+st.subheader("📉 Average Discount by Category")
+
+average_discount = (
+    df.groupby("Category")["discountPercent"]
+      .mean()
+      .round(2)
+      .reset_index()
+)
+
+fig2 = px.bar(
+    average_discount,
+    x="discountPercent",
+    y="Category",
+    orientation="h",
+    title="Average Discount by Category"
+)
+
+st.plotly_chart(
+    fig2,
+    use_container_width=True
+)
+
+# ==================================================
+# STOCK ANALYSIS
+# ==================================================
+
+st.subheader("📦 Stock Analysis by Category")
+
+stock_category = (
+    df.groupby("Category")["quantity"]
+      .sum()
+      .reset_index()
+)
+
+fig4 = px.bar(
+    stock_category,
+    x="Category",
+    y="quantity",
+    title="Total Stock Available by Category"
+)
+
+st.plotly_chart(
+    fig4,
+    use_container_width=True
+)
+
+
+# ==================================================
+# LOW STOCK PRODUCTS
+# ==================================================
+
+st.subheader("⚠️ Low Stock Products")
+
+low_stock = (
+    df[df["quantity"] < 10]
+    .sort_values(
+        by="quantity"
+    )
+)
+
+st.dataframe(
+    low_stock[
+        ["name", "Category", "quantity"]
+    ],
+    use_container_width=True
+)
