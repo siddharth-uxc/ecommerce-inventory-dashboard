13de7cf10fa65b1a9af553e3078bb96b94fd50fa
siddharth_uxc <siddharthjhamail@gmail.com>
2026-07-15 22:22:00 +0530

    Add product count by category by category bar chart to dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 # Page configuration
 st.set_page_config(
@@ -17,3 +18,89 @@ df = pd.read_csv("data/zepto_cleaned.csv")
 # Display first five rows
 st.subheader("Dataset Preview")
 st.dataframe(df.head())
+
+
+
+
+# ==================================================
+# KPI CALCULATIONS
+# Calculate important business metrics to display
+# at the top of the dashboard.
+# ==================================================
+
+total_products = len(df)
+
+total_categories = df["Category"].nunique()
+
+out_of_stock = df["outOfStock"].sum()
+
+average_selling_price = round(
+    df["discountedSellingPrice"].mean(),
+    2
+)
+
+
+
+
+
+
+# Create four columns for KPI cards
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric(
+    "📦 Total Products",
+    total_products
+)
+
+col2.metric(
+    "📂 Categories",
+    total_categories
+)
+
+col3.metric(
+    "❌ Out of Stock",
+    out_of_stock
+)
+
+col4.metric(
+    "💰 Avg Selling Price",
+    f"₹{average_selling_price}"
+)
+
+
+
+# ==================================================
+# PRODUCT COUNT BY CATEGORY
+# Count the number of products in each category.
+# ==================================================
+
+category_count = (
+    df["Category"]
+    .value_counts()
+    .reset_index()
+)
+
+category_count.columns = [
+    "Category",
+    "Product Count"
+]
+
+
+
+
+# ==================================================
+# BAR CHART
+# ==================================================
+
+fig = px.bar(
+    category_count,
+    x="Product Count",
+    y="Category",
+    orientation="h",
+    title="Products Available in Each Category"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
